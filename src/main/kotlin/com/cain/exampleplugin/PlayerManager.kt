5c2ee8c0a6b71c1992262341e1f9110d10ab18bd
Cain <93525547+Cain873@users.noreply.github.com>
2021-11-02 16:16:11 +0900

    マイニングレベル追加

--- src/main/kotlin/com/cain/exampleplugin/PlayerManager.kt
+++ src/main/kotlin/com/cain/exampleplugin/PlayerManager.kt
@@ -5,6 +5,8 @@ import net.md_5.bungee.api.ChatMessageType
 import net.md_5.bungee.api.chat.BaseComponent
 import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.entity.Player
+import org.w3c.dom.Text
+import kotlin.math.min
 
 object PlayerManager {
     private val playerLevels: MutableMap<Player, Int> = mutableMapOf()
@@ -15,7 +17,15 @@ object PlayerManager {
 
     fun addLevel(player: Player, amount: Int) {
         playerLevels[player] = playerLevels[player]?.plus(amount) ?: 0
-        val exp = playerLevels[player] ?: 0
+        var exp = playerLevels[player] ?: 0
+        var miningLevel = 1
         player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent("MiningExp: &a${exp}".colored()))
+        if(exp == 1000) {
+            miningLevel++
+            player.spigot().sendMessage(ChatMessageType.CHAT, TextComponent("&9=================================================".colored()))
+            player.spigot().sendMessage(ChatMessageType.CHAT, TextComponent("&bMiningLevelが&a1&b上がりました（現在&a${miningLevel}&bレベル）".colored()))
+            player.spigot().sendMessage(ChatMessageType.CHAT, TextComponent("&9=================================================".colored()))
+            exp -= 1000
+        }
     }
 }
